3cffd8c67646dc190d2a30523f01ae45c6e3ae0d
Shavynaub <saubeeluck26@uwcchina.org>
2026-09-04 13:41:51 +0800

    Simple two values calculator

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
-print("Hello World")
+opp = input("Enter 1 of the following operations: + - * /")
+num1 = float(input("Enter your first number"))
+num2 = float(input("Enter your 2nd Number"))
+
+if opp == "+":
+    print(num1 + num2)
+elif opp == "-":
+    print(num1-num2)
+elif opp == "*":
+    print(num1 * num2)
+elif opp == "/":
+    print(num1/num2)
+else:
+    print("Invalid operation")
